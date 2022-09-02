9788de7ed421afaa75c0fa4221677b116b271260
HanZawNyein <hanzawnyineonline@gmail.com>
2022-09-02 20:14:01 +0630

    create post model and admin register

---
+++ blog/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+# admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','status','publish')
+    list_filter =('created','publish','updated')
+    search_fields = ('title','body')
+    prepopulated_fields = {'slug':('title',)}
+    raw_id_fields = ('author',)
+    ordering =('status','publish')
---
+++ blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+# create-your-models-here
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,on_delete=models.CASCADE)
+    body = models.TextField()
+    publish = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('publish','Published')
+    )
+    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+
+    def __str__(self) -> str:
+        return self.title
